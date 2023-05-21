e76662e89ad4db4e5ff4f0f5bcecba98365967c4
Arsenyhlimanc243 <112560947+Arsenyhlimanc243@users.noreply.github.com>
2023-05-21 15:33:22 +0300

    Сделал название переменных более понятными

--- main (1).py
+++ main (1).py
@@ -5,13 +5,13 @@ import requests
 from dotenv import load_dotenv
 
 
-def shorten_link(bitly_token, long_url):
+def shorten_link(bitly_token, parser):
     bitly_url = "https://api-ssl.bitly.com/v4/shorten"
     headers = {
         "Authorization": f"Bearer {bitly_token}",
     }
     payload = {
-        "long_url": long_url,
+        "long_url": parser,
     }
     response = requests.post(bitly_url, headers=headers, json=payload)
     response.raise_for_status()
@@ -41,17 +41,17 @@ def count_clicks(bitly_token, bitlink):
 if __name__ == "__main__":
     load_dotenv()
     bitly_token = os.environ["BITLY_TOKEN"]
-    long_url = argparse.ArgumentParser(description="Ссылка")
-    long_url.add_argument("--url", help="Введите ссылку:")
-    url_args = long_url.parse_args()
-    parsed_url = urlparse(url_args.url)
+    parser = argparse.ArgumentParser(description="Ссылка")
+    parser.add_argument("--url", help="Введите ссылку:")
+    arguments = parser.parse_args()
+    parsed_url = urlparse(arguments.url)
     parsed_bitlink = f'{parsed_url.netloc}{parsed_url.path}'
     try:
         if is_bitlink(bitly_token, parsed_bitlink):
            clicks_count = count_clicks(bitly_token, parsed_bitlink)
            print(clicks_count) 
         else:
-            bitlink = shorten_link(bitly_token, url_args.url)
+            bitlink = shorten_link(bitly_token, arguments.url)
             print('Битлинк', bitlink)
     except requests.exceptions.HTTPError:
         print("Неверная ссылка или токен")
